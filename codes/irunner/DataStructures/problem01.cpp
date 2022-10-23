e0ce8c46487e4076a93ac948349b27e235ce0163
0Sigma0 <kisly.stuff@gmail.com>
2022-10-23 17:34:12 +0300

    Add irunner tasks

---
+++ codes/irunner/DataStructures/problem01.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main() {
+    ifstream input("input.txt");
+    int n, q;
+    input >> n >> q;
+    cout << n << " " << q;
+
+
+}

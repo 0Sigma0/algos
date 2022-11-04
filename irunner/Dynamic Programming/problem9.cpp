9f44f8e30ff79ee7fc76620e1f624c172d6cc46d
0Sigma0 <kisly.stuff@gmail.com>
2022-11-04 19:04:31 +0300

    update problem9 in dynamic programming

--- irunner/Dynamic Programming/problem9.cpp
+++ irunner/Dynamic Programming/problem9.cpp
@@ -11,13 +11,16 @@ int LDS(int line[], int n) {
     
     for(int i=0; i<n; i++) {
         for(int j=0; j<i; j++) {
+            int kappa;
+            if(line[j] != 0) kappa = line[i] % line[j];
+            else continue;
             if(line[i] % line[j] == 0 && 1 + f[j] > f[i])
                 f[i] = 1 + f[j];
         }
     }
     
     for(int i=0; i<n; i++) {
-        if(f[i] > ans)
+        if(f[i] >= ans)
             ans = f[i];
     }
     return ans + 1;
@@ -25,15 +28,27 @@ int LDS(int line[], int n) {
 
 int main() {
     int n;
+    int add = 0;
     std::ifstream input("input.txt");
     input >> n;
     int *line = new int[n];
     for(int i=0; i<n; i++) {
-        input >> line[i];
+        int temp;
+        input >> temp;
+        line[i] = abs(temp);
+    }
+    
+    for(int i=0; i<n; i++) {
+        if(line[i] == 0) {
+            add = 1;
+            break;
+        }
     }
     std::sort(line, line + n);
     std::ofstream output("output.txt");
-    output << LDS(line, n);
+    output << LDS(line, n) + add;
     input.close();
     output.close();
 }
+
+

64726c267765b8181d6eeb9bbd6fb1e44225667e
0Sigma0 <kisly.stuff@gmail.com>
2022-11-20 18:31:55 +0300

    add pronblem06 in data structures and graphs

---
+++ irunner/Data Structures and Graphs/problem06/sol.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <vector>
+
+int main() {
+    int numOfNodes, numOfEdges;
+    std::ifstream input("input.txt");
+    std::ofstream output("output.txt");
+    input >> numOfNodes >> numOfEdges;
+    std::vector<std::vector<int>> AdjList(numOfNodes+1);
+    for(int i=0; i<numOfEdges; i++) {
+        int u, v;
+        input >> u >> v;
+        AdjList[u].push_back(v);
+        AdjList[v].push_back(u);
+    }
+    for(int i=1; i<numOfNodes+1; i++) {
+        output << AdjList[i].size();
+        for(auto x : AdjList[i]) {
+            output << " " << x;
+        }
+        output << '\n';
+    }
+}

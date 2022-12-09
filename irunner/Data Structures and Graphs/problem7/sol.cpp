be2ccc287267bcb0f22bdbe149a39d2d6a70a066
0Sigma0 <kisly.stuff@gmail.com>
2022-12-09 17:46:45 +0300

    updated sol.cpp of problem7 in data structures and graphs

--- irunner/Data Structures and Graphs/problem7/sol.cpp
+++ irunner/Data Structures and Graphs/problem7/sol.cpp
@@ -1,73 +1,74 @@
 #include <fstream>
 #include <vector>
-#include <iostream>
-#include <set>
 #include <algorithm>
+
+bool is_bipartite = true;
+
+int invert(int c) {
+    return c == 1 ? 2 : 1;
+}
+
+void dfs(std::vector<std::vector<int>> &AdjList, std::vector<int> &color, int c, int v, std::ofstream &output) {
+    color[v] = c;
+    for (int u : AdjList[v]) {
+        if (color[u] == 0) {
+            dfs(AdjList, color, invert(c), u, output);
+        } else if (color[u] == c) {
+            output << "NO";
+            exit(0);
+        }
+    }
+}
 
 int main() {
     std::ifstream input("input.txt");
     std::ofstream output("output.txt");
-    int numOfStudents = 0;
-    input >> numOfStudents;
-    std::vector<std::vector<int>> AdjList(numOfStudents + 1);
-    for(int i = 1; i < numOfStudents + 1; i++) {
-        for(int j = 1; j < numOfStudents + 1; j++) {
+    int n = 0;
+    input >> n;
+    std::vector<bool> visited(n+1, false);
+    std::vector<bool> part(n+1, false);
+    std::vector<std::vector<int>> AdjList(n + 1);
+    std::vector<int> color(n + 1);
+
+    //inversion of the input matrix
+    for(int i = 1; i < n + 1; i++) {
+        color[i] = 0;
+        for(int j = 1; j < n + 1; j++) {
             int k;
             input >> k;
-            if(k || i == j) AdjList[i].push_back(j); 
+            if(!k && (i != j)) AdjList[i].push_back(j); 
         }
     }
-    std::vector<int> numOfPairings(numOfStudents + 1);
-    std::vector<int> numOfSamePairings(numOfStudents + 1);
-    std::set<int> tempSet;
-    for(int i = 1; i < numOfStudents + 1; i++) {
-        for(int j = 1; j < numOfStudents + 1; j++) {
-            if(AdjList[i] == AdjList[j] && i != j) {
-                numOfPairings[i]++;
-            }
+    
+    for(int v = 1; v < n + 1; v++) {
+        if(color[v] == 0) {
+            dfs(AdjList, color, 1, v, output);
         }
     }
-    std::cout << "numOfPairings: ";
-    for(int i = 1; i < numOfStudents + 1; i++) std::cout << numOfPairings[i] << " ";
-    std::cout << "\n";
-    for(int i = 1; i < numOfStudents + 1; i++) {
-        tempSet.insert(numOfPairings[i]);
+    
+    std::vector<int> sizeOfBipartiteParts(2);
+    for(int i = 0; i < n; i++) {
+        if(color[i+1] == 1) sizeOfBipartiteParts[0]++;
+        else sizeOfBipartiteParts[1]++;
     }
-    if(tempSet.size() > 2) {
-        output << "NO";
-        return 0;
-    }
-    for(int i = 1; i < numOfStudents + 1; i++) {
-        for(int u : tempSet) {
-            if(numOfPairings[i] == u) numOfSamePairings[u]++;
-        }
-    }
-    std::cout << "numOfSamePairings: ";
-    for(int i = 1; i < numOfStudents + 1; i++) std::cout << numOfSamePairings[i] << " ";
-    std::cout << "\n";
-    std::vector<int> bruh;
-    for(int u : numOfSamePairings) {
-        if(u != 0) bruh.push_back(u);
-    }
-    std::cout << "bruh: ";
-    for(int u : bruh) std::cout << u << " ";
-    int max = *max_element(bruh.begin(), bruh.end());
-    int min = *min_element(bruh.begin(), bruh.end());
+    
+    int max = *max_element(sizeOfBipartiteParts.begin(), sizeOfBipartiteParts.end());
+    int min = *min_element(sizeOfBipartiteParts.begin(), sizeOfBipartiteParts.end());
+    
     if((float)max / min > 2) {
         output << "NO";
-    } else {
-        if(numOfStudents == 2){ 
-            output << "YES\n1";
-            return 0;
+    }
+    else {
+        bool done = false;
+        for(int i = 1; i < n + 1; i++) {
+            if(sizeOfBipartiteParts[0] == max && color[i] == 1) {
+                
+                output << i << " ";
+                done = true;
+            }
+            else if(!done && color[i] == 2) output << i;
         }
-        int kappa = 0;
-        for(int i = 1; i < numOfStudents+1; i++) { 
-            if(numOfSamePairings[i] == max) kappa = i;
-        }
-        output << "YES\n";
-        for(int i = 1; i < numOfStudents + 1; i++) {
-            if(numOfPairings[i] == kappa) output << i << " ";
-        }
+        output << "\nYES";
     }
 }
 
